f38b15d6f5ea3b055336a46ee950fd7e2d1e2441
Yael <yaelbercow@gmail.com>
2016-06-25 16:11:27 -0400

    wrote 3 functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Function #1//
+var x = 3;
+var y = 4
+function mathOne(x,y) {
+	return x+y;
+}
+mathOne(x,y);
+
+//Function #2//
+var x = 12;
+if (x<10) {
+	greeting = "less than 10";
+}	else {
+	greeting = "equal or more than 10";
+}
+
+//Function #3//
+var num = prompt('what is your lucky number?');
+var number = Number(num);
+function newLuckyNumber(number) {
+	return number + 3 
+}
+
+newLuckyNumber(3)
